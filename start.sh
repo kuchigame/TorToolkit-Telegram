4007223e4426b6aa7d19d365282e34b14c5f8d6c
kuchigame <66659933+kuchigame@users.noreply.github.com>
2021-09-23 17:03:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-gunicorn tortoolkit:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
+gunicorn tortoolkit:start_server --bind 127.0.0.1:$PORT --worker-class aiohttp.GunicornWebWorker & python3 -m tortoolkit
